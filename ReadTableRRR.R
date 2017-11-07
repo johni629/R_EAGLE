e15fb1ced48d82d96adf3fa62328748636fb9955
JOHNI MIAH <johnijnu29@gmail.com>
2017-11-07 17:53:29 +0600

    CLASS 2

--- ReadTableRRR.R
+++ ReadTableRRR.R
@@ -2,7 +2,7 @@ read.table("bio_data_forest.csv")
 read.csv("bio_data_forest.csv")
 summary("bio_data_forest.csv")
 
-
+# Matrix_Data farame
 x<-matrix(c(4,7,3,8,9,2), nrow=2)
 x
 x[2,2]
@@ -13,3 +13,17 @@ mat_1
 df_1 <- data.frame(mat_1)
 names(df_1) <- c("var1", "var2","var3", "var4")
 head(df_1)
+df_1$var2
+df_1[2:3, 3:4]
+
+# Indexing_Vector
+
+x<- seq(1,100, by=2.5)
+x
+x[5]
+x[4:10]
+x[length(x)]
+x[-2]
+x>20
+(x<=10)|(x>=30)
+x[x<10| x>30]
